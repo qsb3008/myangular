1a4d89679319096aca5e992b5748c9549a294c3c
qsb3008 <qsb3008@163.com>
2018-11-21 16:08:48 +0800

    hero editor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class HeroesComponent implements OnInit {
 
   constructor() { }
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 
   ngOnInit() {
     console.log('生命钩子函数-ngOnInit')

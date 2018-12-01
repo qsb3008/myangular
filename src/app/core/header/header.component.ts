3cf28d628149bb0d298e83cbbece08c136bbfb92
qsb3008 <qsb3008@163.com>
2018-12-01 12:13:37 +0800

    :checkered_flag: 子组件事件传播到父组件再触发

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  // output出去一个叫toggle的EventEmitter
+  // 因为不需要返回任何参数，只是知道被返回出去就行了，所以返回值可以是<void>
+  @Output () toggle = new EventEmitter<void>()
   constructor() { }
 
   ngOnInit() {
   }
 
+  openSidebar () {
+    // 发射
+    // 按钮在header组件，他不知道将会在哪里被触发，所以要发送出去，在接受的地方再触发动作
+    this.toggle.emit()
+  }
 }
